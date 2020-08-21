4488a4d62e148280e3bca6b3f52d06055e43edb6
Alison Appling <aappling@usgs.gov>
2020-08-20 21:41:56 -0400

    update data getting and data prep for data changes

--- scripts/data_getting.R
+++ scripts/data_getting.R
@@ -9,7 +9,7 @@ library(tidyverse)
 # to be replaced by ScienceBase release items soon:
 # data/PB-MTL_all_sources_with_predictions.csv
 # data/PB-MTL_results.csv
-# data/PG-MTL_result_matrix_test_lakes_allsources.csv
+# data/PG-MTL_result_matrix_test_lakes_all_sources.csv
 # data/PG-MTL_result_matrix_test_lakes_ensemble_sources.csv
 # data/PG-MTL_result_matrix_test_lakes_single_sources.csv
 # data/PG-MTL9_305Lakes_PredictedRMSEStats_metadata.csv
@@ -25,29 +25,29 @@ library(tidyverse)
 # source_pgdl_outputs.zip -> data/source_pgdl_outputs/* # 145 feather files, one per source lake
 example_sites <- dir('data/examples/mtl_outputs_for_fig') # use these as a guide for what to download from SB
 
-# Scripted downloads from SB
+#### Scripted downloads from SB ####
+
 library(sbtools)
 sbtools::authenticate_sb('aappling@usgs.gov')
-eval_files <- sprintf('%s_evaluation.csv', c('pb0','pball','pbmtl','pgmtl','pgmtl9'))
-sbtools::item_file_download(
-  sb_id='5ebe577782ce476925e44b32',
-  names=eval_files,
-  destinations=sprintf('data/%s', eval_files),
-  overwrite_file = TRUE)
+
+# lake metadata / spatial data
 lake_files <- c('lake_metadata.csv', '01_spatial.zip')
+if(!dir.exists('data/zips')) dir.create('data/zips')
 sbtools::item_file_download(
   sb_id='5ebe564782ce476925e44b26',
   names=lake_files,
-  destinations=sprintf('data/%s', lake_files),
+  destinations=c('data/lake_metadata.csv', 'data/zips/01_spatial.zip'),
   overwrite_file=TRUE)
 lake_metadata <- readr::read_csv('data/lake_metadata.csv', col_types=cols())
-# also available: data/01_spatial/study_lakes.shp # don't need it [yet]
+unzip('data/zips/01_spatial.zip', exdir='data/spatial')
+
+# predictions for example sites
+if(!dir.exists('data/zips')) dir.create('data/zips')
+if(!dir.exists('data/predictions')) dir.create('data/predictions')
 lapply(example_sites, function(site_id) {
   group <- lake_metadata %>%
     filter(site_id == !!site_id) %>%
     pull(group_id)
-  if(!dir.exists('data/zips')) dir.create('data/zips')
-  if(!dir.exists('data/predictions')) dir.create('data/predictions')
   for(model in c('pb0','pball','pgmtl','pgmtl9')) {
     zipfile <- sprintf('data/zips/%s_predictions_%s.zip', model, group)
     if(!file.exists(zipfile)) {
@@ -62,3 +62,11 @@ lapply(example_sites, function(site_id) {
     }
   }
 })
+
+# evaluation files
+eval_files <- sprintf('%s_evaluation.csv', c('pb0','pball','pbmtl','pgmtl','pgmtl9'))
+sbtools::item_file_download(
+  sb_id='5ebe577782ce476925e44b32',
+  names=eval_files,
+  destinations=sprintf('data/%s', eval_files),
+  overwrite_file = TRUE)
--- scripts/data_prep.R
+++ scripts/data_prep.R
@@ -3,9 +3,9 @@
 library(feather)
 library(tidyverse)
 
-#### Read ####
+#### Utils ####
 
-# Define reader functions
+# Define reader functions for files from SB
 read_preds_feather <- function(preds_file) {
   feather::read_feather(preds_file) %>%
     pivot_longer(cols=starts_with('Depth_'), names_to='depth', names_prefix='Depth_', 
@@ -18,90 +18,110 @@ read_preds_csv <- function(preds_file) {
     pivot_longer(cols=starts_with('temp_'), names_to='depth', names_prefix='temp_', 
                  names_transform=list(depth=as.numeric), values_to='temp_c') 
 }
-# new format from Jared as of 7/6/20
+# reader for example files from Jared as of 8/6/20
 read_preds_mtl_outputs_for_fig <- function(preds_file) {
   feather::read_feather(preds_file) %>%
-    pivot_longer(cols=-index, names_to='date', names_transform=list(date=as.Date), values_to='temp_c') %>%
-    mutate(depth=as.numeric(index)) %>%
-    select(-index)
+    pivot_longer(cols=-index, names_to='depth', names_transform=list(depth=as.numeric), values_to='temp_c') %>%
+    mutate(date=as.Date(index, '%Y-%m-%d')) %>%
+    select(date, depth, temp_c)
 }
-  
+
+#### Read ####
+
 # Read lake metadata
-lake_metadata_full <- readr::read_csv('data/source_metadata.csv')
-lake_metadata <- readr::read_csv('data/lake_metadata.csv')
-#lake_spatial <- sf::read_sf('data/01_spatial/study_lakes.shp') # don't need it [yet]
+lake_metadata <- readr::read_csv('data/lake_metadata.csv', col_types=cols())
+lake_spatial <- sf::read_sf('data/spatial/study_lakes.shp')
+# temporarily supplement with data from Drive; these columns should be in lake_metadata.csv in future. we also need the properties for source lakes, which are omitted from the 2221
+source_metadata <- read_csv('data/old/source_metadata.csv') %>% # we shouldn't have to use this file
+  select(site_id, fullname, max_depth, surface_area, n_obs)
+lake_metadata_full <- read_csv('data/PG-MTL9_Expanded_2221Lakes_wMetadata.csv') %>%
+  slice(-n()) %>% # some trailing line in the Sheet
+  mutate(site_id = sprintf('nhdhr_%s', target_id)) %>%
+  select(site_id, fullname, max_depth, surface_area, n_obs) %>%
+  bind_rows(source_metadata)
 
 # Read PB results from Jordan via Teams
-pball_mtl_305 <- read_csv('data/GLM_metamodel_predicted_sources_glm_transfer_pball_test_lakes.csv') %>%
-  transmute(target_id=sprintf('nhdhr_%s', `target_id(nhdhr)`),
-            source_id=sprintf('nhdhr_%s', `best_predicted_site_id (nhdhr)`),
-            predicted_rmse=predicted_rmse)
-pball_eval_44225 <- read_csv('data/RMSE_transfer_test_extended_glm.csv') %>%
-  mutate(target_id = gsub('test_', '', target_id))
-pball_eval_305 <- pball_eval_44225 %>%
-  right_join(pball_mtl_305, by=c('target_id','source_id'))
+# pball_mtl_305 <- read_csv('data/old/GLM_metamodel_predicted_sources_glm_transfer_pball_test_lakes.csv') %>%
+#   transmute(target_id=sprintf('nhdhr_%s', `target_id(nhdhr)`),
+#             source_id=sprintf('nhdhr_%s', `best_predicted_site_id (nhdhr)`),
+#             predicted_rmse=predicted_rmse)
+# pball_eval_44225 <- read_csv('data/old/RMSE_transfer_test_extended_glm.csv') %>%
+#   mutate(target_id = gsub('test_', '', target_id))
+# pball_eval_305 <- pball_eval_44225 %>%
+#   right_join(pball_mtl_305, by=c('target_id','source_id')) # target_id, source_id, [actual]rmse, predicted_rmse for the 305 targets and their top source (only)
+# can now get this in pbmtl_train_44225
 
-# Read PGMTL results from Jared
-pbmtl_train_305 <- readr::read_csv('data/PB-MTL_all_sources_with_predictions.csv', na='N/A') # new 7/6/20
+# Read P[B/G]MTL[9] results from Jared
+# predicted and actual RMSEs for all 44k source-target pairs
+pbmtl_train_44225 <- readr::read_csv('data/PB-MTL_all_sources_with_predictions.csv', na='N/A') %>% # 44k source-target pairs, predicted and actual rmses
+  rename(actual_rmse = transfer_rmse)
+pgmtl_train_44225 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_all_sources.csv', na='N/A') %>% # junk (NAs) in columns 13 and 14
+  select(target_id, source_id, actual_rmse = rmse, predicted_rmse) # includes metafeatures, but I'm taking them out because they should be computable from lake_metadata_full and eventually lake_metadata
 
-# pgmtl_train_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_all_sources_200714.csv', na='N/A') %>%
-#   rename(rmse_predicted=predicted_rmse); pgmtl_train_305_date <- '200714'
-# pgmtl_train_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_all_sources_200622.csv', na='N/A'); pgmtl_train_305_date <- '200622'
-pgmtl_train_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_all_sources_200426.csv', na='N/A') %>%
-  mutate(rmse_predicted=NA); pgmtl_train_305_date <- '200426'
+# More results from Jared
+# I think we don't actually need these files because pred+obs RMSEs are in the pxmtl_train_44225 files and metafeatures are in lake_metadata_full.
+# The following files contain the selected source[s] for each target, predicted and observed RMSEs, and metafeatures used in the model (but only metafeatures for the pg models, not pb).
+# spearman / median spearman is the correlation coefficient, and meta_rmse is
+# the RMSE, for the 145 predictions vs observations of RMSE for the source lakes
+# applied to a given target.
+# pbmtl_eval_305 <- readr::read_csv('data/PB-MTL_results.csv', na='N/A') # assessments of PBMTL split up by 305 target_ids: spearman, meta_rmse, and others
+# pgmtl_eval_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_single_sources.csv', na='N/A') %>% # assessments of PGMTL split up by 305 target_ids: meta_rmse, median spearman (this is actually just spearman); also 
+#   slice(-c(n()-1, n())) # These dangling RMSE values are the Sheets-computed stats of all values above them
+# pgmtl9_eval_3050 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_ensemble_sources.csv', na='N/A') # 3050 rows, one for each of the top 9 source-target pairs and one for the ensemble for each of 305 targets
+# pgmtl9_evalplus_305 <- readr::read_csv('data/PG-MTL9_305Lakes_PredictedRMSEStats_metadata.csv', na='N/A') %>% # 305+ rows, including many lake properties
+#   slice(-(n() + (-2:0))) %>% # This dangling RMSE value is the Sheets-computed median of all values above it
 
-pgmtl_eval_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_single_sources.csv', na='N/A') %>% # updated 7/6/20
-  slice(-n()) # This dangling RMSE value is the Sheets-computed median of all values above it
-pgmtl9_eval_305 <- readr::read_csv('data/PG-MTL_result_matrix_test_lakes_ensemble_sources.csv', na='N/A') # updated 7/6/20
-# I think pgmtl9_evalplus_305 replaces jw_all_eval_305
-pgmtl9_evalplus_305 <- readr::read_csv('data/PG-MTL9_305Lakes_PredictedRMSEStats_metadata.csv', na='N/A') %>% # new 7/6/20
-  slice(-n()) # This dangling RMSE value is the Sheets-computed median of all values above it
-# jw_all_eval_305 <- readr::read_csv('data/305_lakes_results.csv') # NOT updated 7/6/20 # includes predicted MTL and MLT9 RMSEs and lake metadata for those 305 lakes
+# Read evaluation results from ScienceBase
+pb0_eval_2366 <- read_csv('data/pb0_evaluation.csv') %>% mutate(model = 'pb0') # 1916 + 305 + 145
+pball_eval_450 <- read_csv('data/pball_evaluation.csv') %>% mutate(model = 'pball') # 305 + 145
+pbmtl_eval_305 <- read_csv('data/pbmtl_evaluation.csv') %>% mutate(model = 'pbmtl') # 305
+pgmtl_eval_2221 <- read_csv('data/pgmtl_evaluation.csv') %>% mutate(model = 'pgmtl') # 1916 + 305
+pgmtl9_eval_2221 <- read_csv('data/pgmtl9_evaluation.csv') %>% mutate(model = 'pgmtl9') # 1916 + 305
 
-# Read results from ScienceBase
-pb0_eval_2364 <- read_csv('data/pb0_evaluation.csv')
-# pball_eval_145 <- read_csv('data/pball_evaluation.csv') # would rather have the 305 test lakes (best is from Teams for now)
-pbmtl_eval_305 <- read_csv('data/pbmtl_evaluation.csv')
-pgmtl_eval_2216 <- read_csv('data/pgmtl_evaluation.csv')
-pgmtl9_eval_2216 <- read_csv('data/pgmtl9_evaluation.csv')
-
-# Combine model eval results
-all_eval_305 <-  transmute(pb0_eval_2364, target_id=site_id, pb0_rmse=rmse) %>%
-  left_join(select(pgmtl_eval_2216, target_id=site_id, pgmtl_rmse=rmse), by='target_id') %>% #  pgmtl_source_id=source_id
-  left_join(select(pball_eval_305, target_id, pball_source_id=source_id, pball_rmse=rmse), by='target_id') %>% # Teams file for now
-  left_join(select(pgmtl9_eval_2216, target_id=site_id, pgmtl9_rmse=rmse), by='target_id') %>%
-  filter(!is.na(pball_rmse), !is.na(pgmtl_rmse))
 
 #### Munge ####
 
-sources_info_partial <- select(pgmtl_train_305, target_id, source_id, rmse, rmse_predicted) %>%
+# site IDs for the 3 classes of lake
+target_lakes <- pbmtl_eval_305$site_id
+source_lakes <- setdiff(pball_eval_450$site_id, target_lakes)
+expand_lakes <- setdiff(pgmtl9_eval_2221$site_id, target_lakes)
+
+# Combine model eval results
+all_eval_2366 <- bind_rows(pb0_eval_2366, pball_eval_450, pbmtl_eval_305, pgmtl_eval_2221, pgmtl9_eval_2221) %>%
+  mutate(model=sprintf('%s_rmse', model)) %>%
+  tidyr::pivot_wider(names_from='model', values_from='rmse')
+
+# prepare info about the sources
+sources_info_partial <- select(pgmtl_train_44225, target_id, source_id, actual_rmse, predicted_rmse) %>%
   left_join(select(lake_metadata_full, site_id, fullname, max_depth, surface_area, n_obs), by=c('source_id'='site_id')) %>%
   group_by(target_id) %>%
   mutate(
-    rank=rank(rmse),
-    rank_predicted=rank(rmse_predicted),
+    rank_actual = rank(actual_rmse),
+    rank_predicted = rank(predicted_rmse),
     top_9 = rank_predicted <= 9,
     rank_category = ifelse(rank_predicted == 1, 'Top 1', ifelse(rank_predicted <= 9, 'Top 9', 'Not Top 9')) %>%
       ordered(levels=c('Top 1', 'Top 9', 'Not Top 9'))) %>%
   ungroup()
+# prepare info about each target's set of sources
 target_summary <- sources_info_partial %>%
   group_by(target_id) %>%
   summarize(
-    rmse_min=min(rmse),
-    rmse_mean=mean(rmse),
-    rmse_median=median(rmse),
-    rank_top_1 = rank[rank_predicted == 1],
-    min_rank_top_9 = min(rank[top_9]),
-    max_rank_top_9 = max(rank[top_9]),
-    mean_rank_top_9 = mean(rank[top_9]),
-    median_rank_top_9 = median(rank[top_9])) %>%
-  ungroup() %>%
+    rmse_min = min(actual_rmse),
+    rmse_mean = mean(actual_rmse),
+    rmse_median = median(actual_rmse),
+    rank_top_1 = rank_actual[rank_predicted == 1],
+    min_rank_top_9 = min(rank_actual[top_9]),
+    max_rank_top_9 = max(rank_actual[top_9]),
+    mean_rank_top_9 = mean(rank_actual[top_9]),
+    median_rank_top_9 = median(rank_actual[top_9]),
+    .groups='drop') %>%
   mutate(
-    site_rank_rmse_mean=rank(rmse_mean),
-    site_rank_rmse_median=rank(rmse_median),
+    site_rank_rmse_mean = rank(rmse_mean),
+    site_rank_rmse_median = rank(rmse_median),
     site_rank_rank_top_1 = rank(rank_top_1, ties.method='first'),
     site_rank_min_rank_top_9 = rank(min_rank_top_9, ties.method='first'),
     site_rank_median_rank_top_9 = rank(median_rank_top_9, ties.method='first'),
     site_rank_mean_rank_top_9 = rank(mean_rank_top_9, ties.method='first'),
     site_rank_max_rank_top_9 = rank(max_rank_top_9, ties.method='first'))
+# parepare a single table with info about each target and each source-target pair (is this merger really needed?)
 sources_info <- sources_info_partial %>% left_join(target_summary, by='target_id')
