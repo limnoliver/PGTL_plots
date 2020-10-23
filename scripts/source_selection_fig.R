cc0b2de795a8f2dbe3a4c5dc4c5d817758e12bbc
Alison Appling <aappling@usgs.gov>
2020-10-23 11:11:46 -0400

    switch from 2 beanplots to 1 boxplot

--- scripts/source_selection_fig.R
+++ scripts/source_selection_fig.R
@@ -2,10 +2,8 @@ library(tidyverse)
 source('scripts/style.R')
 source('scripts/data_prep.R')
 
-#### Metamodel figure ####
+#### Metamodel figures ####
 
-# TODO: plot spearman rank coefficient for each target model in the rank plot, separate y axis?
-# one finding is that spearman ranks were worse for PBMTL than for PGMTL
 plot_rankplot <- function(sources_info, model_type=c('PB','PGDL')) {
   # c(PB='Process-based', PG='Process-guided')[model_type]
   rankplot <- sources_info %>%
@@ -23,44 +21,70 @@ plot_rankplot <- function(sources_info, model_type=c('PB','PGDL')) {
 plot_rankplot(pgmtl_info$sources_info, model_type='PGDL')
 plot_rankplot(pbmtl_info$sources_info, model_type='PB')
 
-# filter(sources_info, top_9) %>%
-#   ggplot(aes(x=site_rank_rmse_median, y=rank)) +
-#   geom_point(alpha=0.8, color='#3a55b4') +
-#   theme_bw()
-# filter(sources_info, rank==1) %>% 
-#   ggplot(aes(x=rmse_predicted, y=median_rank_top_9, color=max_depth)) +
-#   geom_point(alpha=0.8) +
-#   theme_bw()
-# target_summary %>% 
-#   ggplot(aes(x=site_rank_rmse_median, y=min_rank_top_9)) +
-#   geom_errorbar(aes(ymin=min_rank_top_9, ymax=max_rank_top_9)) +
-#   theme_bw()
-# target_summary %>% 
-#   ggplot(aes(x=site_rank_rmse_median, y=min_rank_top_9)) + # site_rank_rmse_median, median_rank_top_9, min_rank_top_9
-#   geom_point(aes(y=min_rank_top_9), color='navy') +
-#   geom_point(aes(y=max_rank_top_9), color='lightblue') +
-#   geom_point(aes(y=median_rank_top_9), color='blue') +
-#   theme_bw() +
-#   ylab('Min, median, and max ranks of 9 selected sources lakes') +
-#   xlab('Target lakes ranked by median RMSE of all source lakes')
-# filter(sources_info, rank_predicted==1) %>%
-#   ggplot(aes(x=site_rank_rmse_median, y=rank)) +
-#   geom_point() +
-#   ylab('Actual rank of the source predicted to be rank 1') +
-#   theme_bw()
+# plot_beanplot <- function(target_summary, model_type='PGDL') {
+#   beanplot_data <- target_summary %>%
+#     select(rank_top_1, min_rank_top_9, max_rank_top_9, median_rank_top_9) %>%
+#     pivot_longer(cols=everything(), names_to='rank_statistic', values_to='rank') %>%
+#     mutate(rank_statistic_label = c(
+#       'rank_top_1'='Predicted best',
+#       'min_rank_top_9'='Actual best\nof 9 predicted best',
+#       'median_rank_top_9'='Actual median\nof 9 predicted best',
+#       'max_rank_top_9'='Actual worst\nof 9 predicted best'
+#     )[rank_statistic] %>% ordered())
+#   beanplot_summary <- beanplot_data %>%
+#     group_by(rank_statistic_label) %>%
+#     summarize(
+#       q25=quantile(rank, 0.25),
+#       q50=quantile(rank, 0.50),
+#       q75=quantile(rank, 0.75),
+#       .groups='drop') %>%
+#     pivot_longer(cols=starts_with('q'), names_to='quantile', names_prefix='q', values_to='rank') %>%
+#     mutate(hline_x=as.numeric(rank_statistic_label))
+#   beanplot <- ggplot(beanplot_data, aes(x=rank_statistic_label)) +
+#     geom_violin(aes(y=rank, fill=rank_statistic), color=NA, trim=FALSE, scale='area') +
+#     scale_fill_manual(
+#       guide='none',
+#       values=c(rank_top_1=pgmtl_colors[['central']], min_rank_top_9=pgmtl9_colors[['dark']],
+#                median_rank_top_9=pgmtl9_colors[['central']], max_rank_top_9=pgmtl9_colors[['light']])) +
+#     geom_errorbarh(
+#       data=filter(beanplot_summary, quantile == '50'),
+#       aes(xmin=hline_x-0.1, xmax=hline_x+0.1, y=rank),
+#       size=0.8, height=0, color='gray80') +
+#     geom_errorbarh(
+#       data=filter(beanplot_summary, quantile != '50'),
+#       aes(xmin=hline_x-0.03, xmax=hline_x+0.03, y=rank),
+#       size=0.8, height=0, color='gray80') +
+#     coord_cartesian(ylim=c(0,145)) + ylim(0, NA) +
+#     xlab(sprintf('%s source model', model_type)) +
+#     ylab(sprintf('Actual rank of %s source model by RMSE', model_type)) +
+#     theme_bw()
+#   beanplot
+#   ggsave(sprintf('figures/metamodel_beanplot_%s.png', model_type), beanplot, width=7, height=4)
+# }
+# plot_beanplot(pgmtl_info$target_summary, model_type='PGDL')
+# plot_beanplot(pbmtl_info$target_summary, model_type='PB')
 
-plot_beanplot <- function(target_summary, model_type='PGDL') {
-  beanplot_data <- target_summary %>%
-    select(rank_top_1, min_rank_top_9, max_rank_top_9, median_rank_top_9) %>%
-    pivot_longer(cols=everything(), names_to='rank_statistic', values_to='rank') %>%
+plot_2model_boxplot <- function(...) {
+  target_summaries <- list(...)
+  merged_summary <- purrr::imap_dfr(target_summaries, function(.x, .y) {
+    mutate(.x, Model=sprintf('%s', .y))
+  }) %>% bind_rows()
+  beanplot_data <- merged_summary %>%
+    select(Model, rank_top_1, min_rank_top_9, max_rank_top_9, median_rank_top_9) %>%
+    pivot_longer(cols=-Model, names_to='rank_statistic', values_to='rank') %>%
     mutate(rank_statistic_label = c(
       'rank_top_1'='Predicted best',
       'min_rank_top_9'='Actual best\nof 9 predicted best',
       'median_rank_top_9'='Actual median\nof 9 predicted best',
       'max_rank_top_9'='Actual worst\nof 9 predicted best'
-    )[rank_statistic] %>% ordered())
+    )[rank_statistic] %>% ordered(levels=c(
+      'Predicted best',
+      'Actual best\nof 9 predicted best',
+      'Actual median\nof 9 predicted best',
+      'Actual worst\nof 9 predicted best'
+    )))
   beanplot_summary <- beanplot_data %>%
-    group_by(rank_statistic_label) %>%
+    group_by(Model, rank_statistic_label) %>%
     summarize(
       q25=quantile(rank, 0.25),
       q50=quantile(rank, 0.50),
@@ -68,70 +92,18 @@ plot_beanplot <- function(target_summary, model_type='PGDL') {
       .groups='drop') %>%
     pivot_longer(cols=starts_with('q'), names_to='quantile', names_prefix='q', values_to='rank') %>%
     mutate(hline_x=as.numeric(rank_statistic_label))
-  beanplot <- ggplot(beanplot_data, aes(x=rank_statistic_label)) +
-    geom_violin(aes(y=rank, fill=rank_statistic), color=NA, trim=FALSE, scale='area') +
-    scale_fill_manual(
-      guide='none',
-      values=c(rank_top_1=pgmtl_colors[['central']], min_rank_top_9=pgmtl9_colors[['dark']],
-               median_rank_top_9=pgmtl9_colors[['central']], max_rank_top_9=pgmtl9_colors[['light']])) +
-    geom_errorbarh(
-      data=filter(beanplot_summary, quantile == '50'),
-      aes(xmin=hline_x-0.1, xmax=hline_x+0.1, y=rank),
-      size=0.8, height=0, color='gray80') +
-    geom_errorbarh(
-      data=filter(beanplot_summary, quantile != '50'),
-      aes(xmin=hline_x-0.03, xmax=hline_x+0.03, y=rank),
-      size=0.8, height=0, color='gray80') +
-    # scale_x_discrete(values=c(
-    #   "Predicted best"=1,
-    #   "Actual best\nof 9 predicted best"=2,
-    #   "Actual median\nof 9 predicted best"=3,
-    #   "Actual worst\nof 9 predicted best"=3.5,
-    # )) +
-    coord_cartesian(ylim=c(0,145)) + ylim(0, NA) +
-    xlab(sprintf('%s source model', model_type)) +
-    ylab(sprintf('Actual rank of %s source model by RMSE', model_type)) +
+  beanplot <- ggplot(beanplot_data, aes(x=rank_statistic_label, linetype=Model)) +
+    geom_boxplot(aes(y=rank), color='black', fill='white') +
+    scale_linetype_manual(
+      'Source model',
+      values=c(`PB`='dotted', `PGDL`='solid')) +
+    # scale_fill_manual(
+    #   'Source model',
+    #   values=c(`PB`=model_colors[['PB-MTL']], `PGDL`=model_colors[['PG-MTL']])) +
+    xlab(NULL) +
+    ylab('Actual rank of source model by RMSE') +
     theme_bw()
-  beanplot
-  ggsave(sprintf('figures/metamodel_beanplot_%s.png', model_type), beanplot, width=7, height=4)
+  ggsave('figures/metamodel_2model_boxplot.png', beanplot, width=7, height=4)
+  return(beanplot)
 }
-plot_beanplot(pgmtl_info$target_summary, model_type='PGDL')
-plot_beanplot(pbmtl_info$target_summary, model_type='PB')
-
-# filter(sources_info, rank_predicted==1) %>%
-#   ggplot(aes(x='MLT-selected source', y=rank)) +
-#   geom_violin(color=NA, fill='darkgray') +
-#   geom_hline(aes(yintercept=median(rank))) +
-#   ylab('Actual rank of the source predicted to be rank 1') +
-#   ylim(0,145) +
-#   theme_bw()
-# filter(pgmtl_info$sources_info, rank_predicted==1) %>% summarize(median(rank_actual))
-# filter(pgmtl_info$sources_info, rank_predicted<=9) %>% summarize(median(rank_actual))
-
-# The sites where the MTL does better tend to be those where all source-target pairs do better.
-
-# Sam: And now circling back to where I think we wanted to go with these figures
-# -- from any of these figures, can we understand for those lakes that had poor
-# transfers, which/how many were because the metamodel performed poorly, and
-# which/how many were because it was not possible to find a good source lake? I
-# guess I would still argue that the metamodel "failed" in these second cases,
-# unless it actually predicted that the transfer was going to be bad. Maybe a
-# figure like: one dot for each 305 lakes, abs(x axis predicted minus actual
-# rank), y axis is actual RMSE of the actual best. Then the four quadrants would
-# be: bottom left - "we were right and the transfer was good"; top left - "we
-# were right but the transfer never would have been good"; top right - "we were
-# wrong, but it doesn't matter because the transfer never would have been good";
-# bottom right - "we were wrong, and the transfer could have been good"
-
-# pgmtl_info$sources_info %>%
-#   filter(rank_predicted <= 1) %>%
-#   ggplot(aes(x=rank_predicted-rank_actual, y=rmse_min)) +
-#   geom_point() +
-#   scale_y_log10() +
-#   theme_bw()
-# sources_info %>%
-#   filter(rank_predicted <= 1) %>%
-#   ggplot(aes(x=rmse_min, y=actual_rmse)) +
-#   geom_abline() +
-#   geom_point() +
-#   theme_bw()
+plot_2model_boxplot(PB=pbmtl_info$target_summary, PGDL=pgmtl_info$target_summary)
